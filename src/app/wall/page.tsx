800702e73068100e5fe8b3478dea245ed0502c02
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-01-21 22:56:09 +0100

    feat: add meta data on wall

--- src/app/wall/page.tsx
+++ src/app/wall/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { HOLD_TYPE_COLORS } from "@/lib/config";
+import { FEET_LABEL, HOLD_TYPE_COLORS } from "@/lib/config";
 import { _holds, _problem } from "@/lib/store";
 import { HoldType } from "@/lib/types";
 import { useAtom } from "jotai";
@@ -43,23 +43,40 @@ export default function Page() {
     }));
 
   return (
-    <svg
-      width={dimensions.width}
-      height={dimensions.height}
-      viewBox={`0 0 ${dimensions.width} ${dimensions.height}`}
-      className="bg-black"
-    >
-      {problemHolds.map((hold) => {
-        return (
-          <polygon
-            key={hold.id}
-            points={hold.pxs!.map((px, i) => `${px},${hold.pys![i]}`).join(" ")}
-            fill={hold.fill}
-            stroke={hold.stroke}
-            strokeWidth={2}
-          />
-        );
-      })}
-    </svg>
+    <div className="relative w-full h-screen bg-black">
+      <svg
+        width={dimensions.width}
+        height={dimensions.height}
+        viewBox={`0 0 ${dimensions.width} ${dimensions.height}`}
+      >
+        {problemHolds.map((hold) => {
+          return (
+            <polygon
+              key={hold.id}
+              points={hold
+                .pxs!.map((px, i) => `${px},${hold.pys![i]}`)
+                .join(" ")}
+              fill={hold.fill}
+              stroke={hold.stroke}
+              strokeWidth={2}
+            />
+          );
+        })}
+      </svg>
+
+      {problem && (
+        <div className="absolute top-10 right-10 text-white flex flex-col gap-1 text-right">
+          <span className="text-3xl font-serif">
+            {problem.name || "Bloc sans nom"}
+          </span>
+          <span className="text-2xl font-mono font-bold">
+            Par {problem.author || "-"}
+          </span>
+          <span className="text-xl font-mono font-bold">
+            {FEET_LABEL[problem.feet]} - {problem.grade}
+          </span>
+        </div>
+      )}
+    </div>
   );
 }
