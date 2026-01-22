e28882c84ca244cca896006661d80d7a20db1536
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-01-22 19:38:54 +0100

    refactor: add rotation to wall text

--- src/app/wall/page.tsx
+++ src/app/wall/page.tsx
@@ -157,7 +157,7 @@ export default function Page() {
       </svg>
 
       {problem && (
-        <div className="absolute top-10 right-10 text-white flex flex-col gap-1 text-right">
+        <div className="absolute top-10 right-0 text-white flex flex-col gap-1 text-center rotate-45">
           <span className="text-3xl font-serif">
             {problem.name || "Bloc sans nom"}
           </span>
